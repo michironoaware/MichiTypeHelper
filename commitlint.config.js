ae7568cfcf735bfa67b0677bac4766777bdb9733
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-11 23:20:52 -0300

    ci: add default scope list and warn on unknown scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,6 +10,7 @@ export default {
         "type-empty": [2, "never"],
         "scope-case": [2, "always", "lower-case"],
         "scope-empty": [0, "always"],
+        "scope-enum": [1, "always", ["internal", "release"]],
         "subject-case": [2, "never", ["sentence-case"]],
         "subject-empty": [2, "never"],
         "header-max-length": [1, "always", 100],
