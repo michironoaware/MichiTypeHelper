74c0549a8b76b43f52c919e21948ed76d622250e
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-12 01:04:06 -0300

    ci: no longer generate non-minified distribution

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,22 +3,22 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
 export default [
-	{
-		input: "src/index.ts",
-		output: {
-			file: "dist/index.js",
-			format: "es",
-			name: "Library",
-		},
-		treeshake: false,
-		strictDeprecations: true,
-		plugins: [
-			typescript({
-				tsconfig: "tsconfig.json",
-				compilerOptions: { outDir: Path.dirname("dist/ts-out") },
-			}),
-		],
-	},
+	//	{
+	//		input: "src/index.ts",
+	//		output: {
+	//			file: "dist/index.js",
+	//			format: "es",
+	//			name: "Library",
+	//		},
+	//		treeshake: false,
+	//		strictDeprecations: true,
+	//		plugins: [
+	//			typescript({
+	//				tsconfig: "tsconfig.json",
+	//				compilerOptions: { outDir: Path.dirname("dist/ts-out") },
+	//			}),
+	//		],
+	//	},
 	{
 		input: "src/index.ts",
 		output: {
