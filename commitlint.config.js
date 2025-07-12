dc4d119347e0ceef6f565966882b4da272f3de76
michironoaware <77251557+michironoaware@users.noreply.github.com>
2025-07-11 23:01:09 -0300

    ci: disable commit header max length to avoid error when executing publish workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,17 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "style", "test"],
+        ],
+        "type-case": [2, "always", "lower-case"],
+        "type-empty": [2, "never"],
+        "scope-case": [2, "always", "lower-case"],
+        "scope-empty": [2, "always"],
+        "subject-case": [2, "never", ["sentence-case"]],
+        "subject-empty": [2, "never"],
+        "header-max-length": [1, "always", 100],
+    },
+};
